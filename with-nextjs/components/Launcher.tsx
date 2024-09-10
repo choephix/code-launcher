fbf88a23523c8310bb59cfd362267183e6fd7109
choephix <choephix@gmail.com>
2024-09-10 14:04:29 +0300

    refactor: Remove ExtraTools component to streamline code and enhance maintainability throughout the project

--- with-nextjs/components/Launcher.tsx
+++ with-nextjs/components/Launcher.tsx
@@ -1,13 +1,11 @@
 import ProjectsList from '@/components/ProjectsList';
-import ExtraTools from './ExtraTools';
-import SmartBar from './SmartBar';
-import ProjectTemplates from './ProjectTemplates';
 import Footer from './Footer';
+import ProjectTemplates from './ProjectTemplates';
+import SmartBar from './SmartBar';
 
 export default function Launcher() {
   return (
     <div className='text-white w-full max-w-screen-md animate-fade-in'>
-      {/* <h1 className='text-3xl font-bold text-center text-blue-400 my-4'>Code Launcher</h1> */}
       <h1 className='text-md font-bold text-center text-blue-400 mb-3 mt-8 font-mono'>
         {'{code:launcher}'}
       </h1>
