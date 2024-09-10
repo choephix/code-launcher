bc2ba5bf39cba514e62626313498312b6f6989f1
Stefan <choephix@gmail.com>
2024-09-10 21:09:36 +0000

    chore: Update styles in globals.css and Launcher component for improved layout and scrollbar appearance across browsers

--- apps/both-with-nextjs/components/Launcher.tsx
+++ apps/both-with-nextjs/components/Launcher.tsx
@@ -5,21 +5,21 @@ import SmartBar from './SmartBar';
 
 export default function Launcher() {
   return (
-    <div className='text-white w-full max-w-screen-md animate-fade-in'>
-      <h1 className='text-md font-bold text-center text-blue-400 mb-3 mt-8 font-mono'>
-        {'{code:launcher}'}
-      </h1>
+    <div className="flex flex-col min-h-screen text-white w-full max-w-screen-md mx-auto animate-fade-in">
+      <div className="flex-grow p-5">
+        <h1 className="text-md font-bold text-center text-blue-400 mb-3 mt-8 font-mono">{'{code:launcher}'}</h1>
 
-      <div className='mb-10'>
-        <SmartBar />
-      </div>
+        <div className="mb-10">
+          <SmartBar />
+        </div>
 
-      <div className='mb-10'>
-        <ProjectTemplates />
-      </div>
+        <div className="mb-10">
+          <ProjectTemplates />
+        </div>
 
-      <div className='mb-10'>
-        <ProjectsList />
+        <div className="mb-10">
+          <ProjectsList />
+        </div>
       </div>
 
       <Footer />
