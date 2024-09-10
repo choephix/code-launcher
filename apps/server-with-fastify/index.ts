f3df236b0ae33873bc2093cfdd487bca6d3e39cc
Stefan <choephix@gmail.com>
2024-09-10 22:40:11 +0000

    chore: Integrate @fastify/static for serving static files and refactor API routes structure in Fastify server setup

--- apps/server-with-fastify/index.ts
+++ apps/server-with-fastify/index.ts
@@ -1,31 +1,43 @@
-import Fastify, { FastifyInstance, FastifyRequest } from 'fastify';
 import { CodeLauncherServerActions } from '@code-launcher/shell-operations';
+import fastifyStatic from '@fastify/static';
+import Fastify, { FastifyInstance, FastifyRequest } from 'fastify';
+import path from 'path';
 
 const fastify: FastifyInstance = Fastify({
   logger: true,
 });
 
-fastify.get('/', async (request, reply) => {
-  return 'Hello Fastify';
+// Serve static files from ../../build-client at the root path
+fastify.register(fastifyStatic, {
+  root: path.resolve('../../build-client'),
+  prefix: '/', // Serve at root path
 });
 
-fastify.get('/ls', async (request, reply) => {
-  const result = await CodeLauncherServerActions.getProjectDirectoriesList();
-  return result;
-});
+// API routes
+fastify.register(
+  (fastify, _, done) => {
+    fastify.get('/ls', async (request, reply) => {
+      const result = await CodeLauncherServerActions.getProjectDirectoriesList();
+      return result;
+    });
 
-fastify.post(
-  '/run-command',
-  async (
-    request: FastifyRequest<{
-      Body: { command: string };
-    }>,
-    reply
-  ) => {
-    const { command } = request.body;
-    const result = await CodeLauncherServerActions.runCommand(command);
-    return result;
-  }
+    fastify.post(
+      '/run-command',
+      async (
+        request: FastifyRequest<{
+          Body: { command: string };
+        }>,
+        reply
+      ) => {
+        const { command } = request.body;
+        const result = await CodeLauncherServerActions.runCommand(command);
+        return result;
+      }
+    );
+
+    done();
+  },
+  { prefix: '/api' }
 );
 
 const start = async () => {
@@ -39,4 +51,3 @@ const start = async () => {
 };
 
 start();
-
