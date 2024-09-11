75fef3bd0942876774ac725142ca608093f9ea05
Stefan <choephix@gmail.com>
2024-09-11 00:06:48 +0000

    chore: Add executable banner and specify binary in package.json for server entry point
    - Enhanced build setup for easier execution and access

--- apps/server-with-fastify/build.js
+++ apps/server-with-fastify/build.js
@@ -7,9 +7,14 @@ esbuild
     platform: 'node',
     target: ['node18'],
     outfile: '../../build/server/index.js',
+    banner: {
+      js: '#!/usr/bin/env node',
+    },
     define: {
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
-      'process.env.CODELAUNCHER_WORKSPACE_PATH': JSON.stringify(process.env.CODELAUNCHER_WORKSPACE_PATH || '/workspaces'),
+      'process.env.CODELAUNCHER_WORKSPACE_PATH': JSON.stringify(
+        process.env.CODELAUNCHER_WORKSPACE_PATH || '/workspaces'
+      ),
     },
   })
   .catch(() => process.exit(1));
