1354e184a730d9627c8c7b441b9eb807d38ea482
Stefan <choephix@gmail.com>
2024-09-11 00:41:49 +0000

    chore: Add @vitejs/plugin-react-swc to package.json and update vite.config.ts to use it for React support in client app

--- apps/client-with-vite/vite.config.ts
+++ apps/client-with-vite/vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import reactSWC from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [reactSWC()],
   build: {
     outDir: path.resolve(__dirname, '../../build/client'),
     emptyOutDir: true,
