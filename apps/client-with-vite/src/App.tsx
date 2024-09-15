91bc5cea658667359a53397e1ea6070c0b332ea6
choephix <choephix@gmail.com>
2024-09-15 13:58:24 +0300

    Refactor Home component and remove Launcher; add TitleThing component

--- apps/client-with-vite/src/App.tsx
+++ apps/client-with-vite/src/App.tsx
@@ -1,9 +1,44 @@
-import Launcher from './components/Launcher';
+import { useEffect } from 'react';
+
+import Footer from '@/components/Footer';
+import ProjectsList from '@/components/ProjectsList';
+import ProjectTemplates from '@/components/ProjectTemplates';
+import SmartBar from '@/components/SmartBar';
+import { TitleThing } from '@/components/TitleThing';
+import { actions, store } from '@/lib/store';
+
+//// Feature Flags
+const hideTemplatesAtProjectCount = 12;
+////
 
 export default function Home() {
+  useEffect(() => {
+    actions.refreshProjects().then(() => {
+      store.uiState.showTemplates = store.projects !== null && store.projects.length < hideTemplatesAtProjectCount;
+    });
+  }, []);
+
   return (
     <main className="bg-gray-900 min-h-screen flex flex-col justify-start items-center animate-fade-in">
-      <Launcher />
+      <div className="flex flex-col min-h-screen text-white w-full max-w-screen-md mx-auto animate-fade-in">
+        <div className="flex-grow p-5">
+          <TitleThing />
+
+          <div className="mb-10">
+            <SmartBar />
+          </div>
+
+          <div className="mb-10">
+            <ProjectTemplates />
+          </div>
+
+          <div className="mb-10">
+            <ProjectsList />
+          </div>
+        </div>
+
+        <Footer />
+      </div>
     </main>
   );
 }
---
+++ apps/client-with-vite/src/components/TitleThing.tsx
@@ -0,0 +1,35 @@
+import { useStore } from "@/lib/store";
+
+//// Feature Flags
+const useDynamicTitle = false;
+////
+
+export function TitleThing() {
+  const { titleContent, titleStyle } = getTitleContentAndStyle();
+
+  return (
+    <h1 className="text-md font-bold text-center text-blue-400 mb-4 mt-8 font-mono" style={titleStyle}>
+      {titleContent}
+    </h1>
+  );
+}
+
+function getTitleContentAndStyle() {
+  const { pathToWorkspaces, activeSmartBarFeature } = useStore();
+
+  // const defaultTitleContent = '{code:launcher}';
+  // const defaultTitleContent = 'code:launcher';
+  const defaultTitleContent = pathToWorkspaces || 'code:launcher';
+
+  if (!useDynamicTitle) {
+    return {
+      titleContent: defaultTitleContent,
+      titleStyle: {},
+    };
+  }
+
+  return {
+    titleContent: activeSmartBarFeature?.bigTitle?.content || defaultTitleContent,
+    titleStyle: activeSmartBarFeature?.bigTitle?.style || {},
+  };
+}
