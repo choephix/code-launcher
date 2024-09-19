bf05f879461d9a4236ad1da0ac67d6d0f2bd384b
choephix <choephix@gmail.com>
2024-09-19 11:35:14 +0300

    Get additional system info (wip) [skip ci]

--- packages/shell-operations/src/lib/system.ts
+++ packages/shell-operations/src/lib/system.ts
@@ -1,4 +1,5 @@
 import os from 'os';
+import fs from 'fs';
 
 interface SystemStats {
   cpuUsage: number | null;
@@ -23,4 +24,36 @@ export function getMemoryAndCPU(): SystemStats {
       memUsage: null,
     };
   }
+}
+
+export function getSystemInfo() {
+  const platform = os.platform();
+
+  const cpuUsage = os.loadavg()[0];
+  const totalMemory = os.totalmem();
+  const freeMemory = os.freemem();
+  const memUsage = (totalMemory - freeMemory) / totalMemory;
+
+  const wslDistroName = process.env.WSL_DISTRO_NAME;
+  const wslInterop = process.env.WSL_INTEROP;
+  const isWSL = process.platform === 'linux' && (wslDistroName || wslInterop);
+
+  // const procVersion = fs.readFileSync('/proc/version', 'utf8').toLowerCase();
+  // const procVersionLowerCase = procVersion.toLowerCase();
+  // if (procVersion.includes('microsoft') || procVersion.includes('wsl')) isWSL = true;
+
+  return {
+    platform,
+    architecture: os.arch(),
+    kernelVersion: os.release(),
+    type: os.type(),
+    userInfo: os.userInfo(),
+    memTotal: totalMemory,
+    memFree: freeMemory,
+    memUsage,
+    cpuUsage,
+    isWSL,
+    wslDistroName,
+    wslInterop,
+  };
 }
