f09f8d2e60e950e41ff2dd2c815a0f4c019de15e
choephix <choephix@gmail.com>
2024-09-19 14:29:18 +0300

    Remove unused legacy project "both-with-nextjs" to reduce clutter

--- apps/both-with-nextjs/app/api/lib/defaultPathToWorkspaces.ts
+++
@@ -1,3 +0,0 @@
-import path from 'path';
-
-export const defaultPathToWorkspaces = path.resolve(process.env.CODELAUNCHER_WORKSPACE_PATH || '.');
--- apps/both-with-nextjs/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Launcher from '@/components/Launcher';
-
-export default function Home() {
-  return (
-    <main className="bg-gray-900 min-h-screen flex flex-col justify-start items-center">
-      <Launcher />
-    </main>
-  );
-}
